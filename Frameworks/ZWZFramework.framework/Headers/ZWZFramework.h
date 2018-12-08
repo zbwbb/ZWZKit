a93457b8d35032befde94c9320224dc8d73d94a1
zbwbb <zhoubinxinna@sina.com>
2018-12-08 21:00:26 +0800

    add libraries

---
+++ Frameworks/ZWZFramework.framework/Headers/ZWZFramework.h
@@ -0,0 +1,19 @@
+//
+//  ZWZFramework.h
+//  ZWZFramework
+//
+//  Created by 周彬 on 2018/12/8.
+//  Copyright © 2018年 周彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZWZFramework.
+FOUNDATION_EXPORT double ZWZFrameworkVersionNumber;
+
+//! Project version string for ZWZFramework.
+FOUNDATION_EXPORT const unsigned char ZWZFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZWZFramework/PublicHeader.h>
+
+#import <ZWZFramework/ZBString.h>
---
+++ Libraries/ZBLibrary/ZBLibrary.h
@@ -0,0 +1,13 @@
+//
+//  ZBLibrary.h
+//  ZBLibrary
+//
+//  Created by 周彬 on 2018/12/8.
+//  Copyright © 2018年 周彬. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZBLibrary : NSObject
+
+@end
